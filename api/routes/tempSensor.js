a2dc2c21e3aefc872b9f6ab18a6a101eafac0d3c
FrankNorq <168375928+FrankNorq@users.noreply.github.com>
2025-05-09 12:26:21 +0200

    Improved the server

--- api/routes/tempSensor.js
+++ api/routes/tempSensor.js
@@ -12,7 +12,7 @@ router.get("/", async (req, res ) => {
 });
 
 router.post("/", async (req, res) => {
-  const { smoke,propane,co2 } = req.body;
+  const { temperature } = req.body;
   const result = await req.pool.query("INSERT INTO temperature (temperature) VALUES ($1) RETURNING *;", [temperature]);
   res.status(201).json(result.rows[0]);
 });
--- models/db.js
+++ models/db.js
@@ -4,8 +4,19 @@ import {Pool} from "pg";
 const pool = new Pool({
     user:"postgres",
     host:"localhost",
-    database:"postgres",
-    password: "",
+    database:"chaschallenge",
+    password: "123",
     port:5432
 });
+
+
+pool.connect()
+    .then(client => {
+        console.log("Ansluten till PostgreSQL-servern!");
+        client.release();
+    })
+    .catch(err => {
+        console.error("Kunde inte ansluta till PostgreSQL-servern:", err);
+    });
+
 export default pool;
